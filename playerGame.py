7d553eea9c3d4df476ed9bb20ebbb1f448cd9533
Facundo Romaniuk <lightpulsar58@gmail.com>
2017-12-07 16:52:31 -0300

    llevando a cabo el jugador

---
+++ playerGame.py
@@ -0,0 +1,23 @@
+
+from playGame import isLetterInWord
+
+
+def playerLetterWrite(letter):
+    
+    while True:
+        isLetterInWord()
+        letterWrite = input("Ingrese una letra: ")
+        if counter in += 1:
+            print("~La palabra contiene esta letra~")
+            
+        else:
+            print("~La palabra no contiene esta letra~")
+            print("~Usted tiene una vida menos~")
+
+def playerLives(lives):
+
+    while True:
+        isLetterInWord()
+        lives = 
+
+
